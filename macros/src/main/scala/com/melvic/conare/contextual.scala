d2969778f3a5db5a17add2e3f9e01a94b0fa5e26
Melvic <melvic.ybanez2014@gmail.com>
2019-12-17 04:25:22 +0800

    WIP: Add support for function envs

--- macros/src/main/scala/com/melvic/conare/contextual.scala
+++ macros/src/main/scala/com/melvic/conare/contextual.scala
@@ -13,33 +13,47 @@ class ContextualMacro(val c: whitebox.Context) {
   import c.universe._
 
   def impl[A](annottees: c.Expr[Any]*): c.Expr[Any] = {
-    val tree = annottees.map(_.tree) match {
+    val tree = annottees.map(_.tree) match {  /*_*/
       case q"$mod def $func[..$tparams](...$params): $ret = $body" :: _ =>
-        q"$mod def $func[..$tparams](...$params)(implicit ..$environment): $ret = $body"
+        val (paramsDecl, envRet) = environment
+        val retTree = constructReturnType(envRet, ret)
+        q"$mod def $func[..$tparams](...$params)(implicit ..$paramsDecl): $retTree = $body"
       case expr => c.abort(c.enclosingPosition, s"Expected: function declaration. Got $expr")
-    }
+    }   /*_*/
+
     c.Expr(tree)
   }
 
   def environment = c.prefix.tree match {
-    case q"new contextual[$tparam]" => c.enclosingClass.children match {
-      case Template(_, _, body) :: _ => body.flatMap {
-        case q"type $typeName = (..$params)" if typeName.toString == tparam.toString =>
-          Some(params.map {
-            case Ident(typ: TypeName) => typeToParam(typ)
-          })
-        case _ => None
-      }.headOption getOrElse {
-        c.abort(c.enclosingPosition, s"Could not find declaration: ${tparam.toString}")
-      }
-    }
+    case q"new contextual[$tparam]" =>
+      def correctTypeName: TypeName => Boolean = _.toString == tparam.toString
+
+      c.enclosingClass.children match {   /*_*/
+        case Template(_, _, body) :: _ => body.flatMap {
+          case q"type $typeName = (..$params)" if correctTypeName(typeName) =>
+            Some((constructTermParams(params), EmptyTree))
+          case q"type $typeName = (..$params) => $ret" if correctTypeName(typeName) =>
+            Some(constructTermParams(params), ret)
+          case _ => None
+        }.headOption getOrElse {
+          c.abort(c.enclosingPosition, s"Could not find declaration: ${tparam.toString}")
+        }
+      }   /*_*/
+
     case expr => c.abort(c.enclosingPosition,
       s"Expected Type Param: type declaration (e.g. type Foo = (Bar, Baz)). Got $expr")
   }
 
-  def typeToParam(typeName: TypeName) = {
-    val paramName = typeName.decodedName.toString
-    val termParam = TermName(paramName.head.toLower + paramName.tail)
-    q"$termParam: $typeName"
+  def constructTermParams(params: List[Tree]) = params.map {
+    case Ident(typeName: TypeName) =>
+      val paramName = typeName.decodedName.toString
+      val termParam = TermName(paramName.head.toLower + paramName.tail)
+      q"$termParam: $typeName"
+  }
+
+  def constructReturnType: (Tree, Tree) => Tree = {
+    case (EmptyTree, ret) => ret
+    case (ret, EmptyTree) => ret
+    case (envRet, funcRet) => tq"$envRet => $funcRet"
   }
 }
