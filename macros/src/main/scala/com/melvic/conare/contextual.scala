5576147360f74deb2aad88388e72e7c75a4c2527
Melvic <melvic.ybanez2014@gmail.com>
2019-12-18 01:11:12 +0800

    Add docstrings

--- macros/src/main/scala/com/melvic/conare/contextual.scala
+++ macros/src/main/scala/com/melvic/conare/contextual.scala
@@ -4,6 +4,20 @@ import scala.annotation.{StaticAnnotation, compileTimeOnly}
 import language.experimental.macros
 import scala.reflect.macros.whitebox
 
+/**
+ * Generates implicit parameters for the annottee based on a given type.
+ * If the macro can locate the definition of the input type, it will deconstruct
+ * the right-hand-side of the definition and provide implicits for it. Otherwise,
+ * an implicit parameter is generated for the type itself.
+ *
+ * Tuples are deconstructed into multiple parts (but not recursively) and implicits
+ * are provided for each part.
+ *
+ * If the RHS of a definition is a function, or the type parameter itself is, its return
+ * type might override the return type of the annottee unless the annottee explicitly
+ * provides a return type, in which case a currying will occur.
+ * @tparam A The type on which the generated environment is based.
+ */
 @compileTimeOnly("enable macro paradise to expand macro annotations")
 class contextual[A] extends StaticAnnotation {
   def macroTransform(annottees: Any*): Any = macro ContextualMacro.impl[A]
@@ -24,6 +38,10 @@ class ContextualMacro(val c: whitebox.Context) {
     c.Expr(tree)
   }
 
+  /**
+   * Constructs the environment based on the type parameter of the annotation.
+   * @return The parameter declarations together with the return type.
+   */
   def environment = c.prefix.tree match {
     case q"new contextual[$tparam]" =>
       def correctTypeName: TypeName => Boolean = _.toString == tparam.toString
